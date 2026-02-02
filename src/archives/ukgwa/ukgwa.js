e23fd24af3c792b78f50efee1749e1bbb4c6f88b
headlesshorse <github-login@headless.horse>
2026-02-02 10:15:04 +0000

    Update endpointIF

--- src/archives/ukgwa/ukgwa.js
+++ src/archives/ukgwa/ukgwa.js
@@ -8,16 +8,11 @@ export const ukgwa = {
   // Timegate and endpoints
   timegate: "https://webarchive.nationalarchives.gov.uk/ukgwa/",
   endpointID: "https://webarchive.nationalarchives.gov.uk/ukgwa/${datetime}id_/${url}",
-  endpointIF: "https://webarchive.nationalarchives.gov.uk/ukgwa/${datetime}if_/${url}",
+  endpointIF: "https://webarchive.nationalarchives.gov.uk/ukgwa/nobanner/${datetime}/${url}",
 
   // Cleaning and filtering rules
   cleaningRules: [],
   excludedPaths: [
     "/static/",
-    "https://cdn.jsdelivr.net",
-    "https://fonts.googleapis.com",
-    "https://fonts.gstatic.com",
-    "https://s3-eu-west-1.amazonaws.com",
-    "https://www.googletagmanager.com",
   ]
 };
