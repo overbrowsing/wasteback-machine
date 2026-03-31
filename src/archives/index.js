dd6c7e022317e82a95fb9f8ef3f8a2d6aaed0ca3
headlesshorse <github-login@headless.horse>
2026-03-31 12:51:07 +0100

    Updae to dynamically import web archive manifests from the current directory

--- src/archives/index.js
+++ src/archives/index.js
@@ -1,29 +1,17 @@
-import { arq } from "./arq/arq.js";
-import { awa } from "./awa/awa.js";
-import { cz } from "./cz/cz.js";
-import { gcwa } from "./gcwa/gcwa.js";
-import { ia } from "./ia/ia.js";
-import { iwa } from "./iwa/iwa.js";
-import { loc } from "./loc/loc.js";
-import { nliwa } from "./nliwa/nliwa.js";
-import { nzwa } from "./nzwa/nzwa.js";
-import { pwa } from "./pwa/pwa.js";
-import { slo } from "./slo/slo.js";
-import { ukgwa } from "./ukgwa/ukgwa.js";
-import { ukwa } from "./ukwa/ukwa.js";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export const archives = {
-  [arq.id]: arq,
-  [awa.id]: awa,
-  [cz.id]: cz,
-  [gcwa.id]: gcwa,
-  [loc.id]: loc,
-  [ia.id]: ia,
-  [iwa.id]: iwa,
-  [nliwa.id]: nliwa,
-  [nzwa.id]: nzwa,
-  [pwa.id]: pwa,
-  [slo.id]: slo,
-  [ukgwa.id]: ukgwa,
-  [ukwa.id]: ukwa
-};
+const currentFolder = path.dirname(fileURLToPath(import.meta.url));
+
+export const archives = {};
+
+for (const folder of fs.readdirSync(currentFolder)) {
+  const filePath = path.join(currentFolder, folder, `${folder}.js`);
+  if (!fs.existsSync(filePath)) continue;
+
+  const module = await import(filePath);
+  const [archive] = Object.values(module);
+
+  if (archive?.id) archives[archive.id] = archive;
+}
